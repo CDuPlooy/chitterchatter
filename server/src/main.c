e4774b8c9cf46341b30232a946fb22befb989dba
RagingGrim <armandconnor.duplooy@gmail.com>
2017-03-15 09:54:14 +0200

    Switched to mnl stable release 0.2.0

--- server/src/main.c
+++ server/src/main.c
@@ -2,33 +2,35 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <assert.h>
 #include "../lib/mnl.h"
 
 #define N_THREADS 25
 
 void *threadHandle(void *data){
 	p_threadInfo ti = data;
-	sleep(5);
-
-	logIt(ti->reserved, threadQueue_dequeue(ti->queue));
-	logIt(ti->reserved, threadQueue_dequeue(ti->queue)); // Log the dequeued message by using logger object ( thread safe )
-
-	// Cleanup? Trampoline function handles this!
+	threadQueue_enqueue(ti->controllerQueue, "Starting!");
 	return NULL;
 }
 
 int main(int argc , char **argv){
-	logger *log = logger_init("log.log"); // Create a logger object which logs to log.log
+	p_threadController tc = threadController_init();
+	assert(tc != NULL);
 
-	logIt(log, "Application started\n");
-	p_threadController tc = threadController_init(); // Create a thread Controller
-	for(size_t i = 0 ; i < N_THREADS ; i++){
-		threadController_pushback(tc, threadHandle, log); // Create a thread which executes on threadHandle and passes log as a reserved object.
-		threadController_messsage(tc, i, "Hello world\n");
-		threadController_messsage(tc, i, "Bye world\n");
+	for(size_t i = 0 ; i < N_THREADS ; i++)
+		threadController_pushback(tc, threadHandle, NULL);
+
+	while(1 == 1){
+		puts("Checking Queue");
+		char *val = threadQueue_dequeue(tc->controllerQueue);
+		puts("Ere");
+		if(val)
+			puts(val);
+		else{
+			puts("No new data! Sleeping for a bit!");
+		}
 	}
 
-	threadController_destroy(tc); // Cleanup of all threads ; ( Disconnects the controller ; threads can keep running though )
-	logger_destroy(log); // Destroy the log object.
+	threadController_destroy(tc);
 	return 0;
 }
